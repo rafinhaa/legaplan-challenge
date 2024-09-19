fd5a9927edf1f8678fde6d308157b5937a738cc7
Rafinhaa <rafinhaa@me.com>
2024-09-19 16:24:40 -0300

    feat: implement create task modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
-'use client';
+"use client";
 
+import { Button } from "@/components/button";
 import { Header } from "@/components/header/header";
+import { TaskModal } from "@/components/task-modal";
 import { Tasks } from "@/components/tasks";
+import { useState } from "react";
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClickShowModal = () => {
+    setShowModal(true);
+  };
 
   return (
     <>
       <Header />
       <main>
         <Tasks />
+        <Button variant="primary" onClick={handleClickShowModal}>
+          Adicionar nova tarefa
+        </Button>
       </main>
+
+      {showModal && <TaskModal onCancel={() => setShowModal(false)} />}
     </>
   );
 };
