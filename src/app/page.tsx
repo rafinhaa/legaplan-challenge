737f3c75938c42ba1ef661a7af4d3e3d58cf495b
Rafinhaa <rafinhaa@me.com>
2024-09-19 12:38:29 -0300

    feat: implement component tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import { Header } from "@/components/header/header";
+import { Tasks } from "@/components/tasks";
 
 const Home = () => {
 
   return (
     <>
       <Header />
+      <main>
+        <Tasks />
+      </main>
     </>
   );
 };
